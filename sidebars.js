c9b3b8868abaea5d5ea95bc0377d670be79ccef5
Crypto Joks <98596722+cryptojoks@users.noreply.github.com>
2023-06-19 13:47:47 -0600

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -57,7 +57,7 @@ module.exports = {
         },
         {
           type: "category",
-          label: "zkEVM Bridge",
+          label: "Antofy Bridge",
           link: {
             type: "generated-index",
           },
